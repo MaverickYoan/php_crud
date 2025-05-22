a94a177b5d5a3558e9f3ee0ebc17b84e8e5b5264
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-05-22 13:14:49 +0200

    db new PDO

--- src/connect.php
+++ src/connect.php
@@ -1,1 +1,15 @@
 <?php
+const SERVER_NAME = "db";
+const DB_NAME = 'project_crud';
+const USERNAME = "test";
+const PASSWORD = "test";
+const PORT = "5432";
+
+try {
+    //code...
+    $db = new PDO("pgsql:host= " . SERVER_NAME . ";port=" . PORT . ";dbname=" . DB_NAME . ";", USERNAME, PASSWORD);
+    echo "connexion réussie";
+} catch (PDOException $e) {
+    //throw $th;
+    echo "échec de connexion : " . $e->getMessage() . "<br>";
+}
--- src/index.php
+++ src/index.php
@@ -8,15 +8,27 @@
     <!-- // - 2 - META -->
     <meta charset=utf-8>
     <meta content="width=device-width,initial-scale=1" name=viewport>
-
+    <link rel="stylesheet" href="style.css">
     <title>php_crud</title><!-- TITRE -->
 </head>
 
 <!-- * BODY -->
 
-<body>
+<body id="content">
     <h1>test</h1>
     <p>créer table sql interns</p>
+
+    <!-- NAVBAR -->
+    <nav class="navbar">
+        <ul class="nav-links" id="navLinks">
+            <li><a class="links" href="#welcome">Home</a></li>
+            <li><a class="links" href="#shop">Shop</a></li>
+            <li><a class="links" href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+
+    <hr />
+
 
 
     <?php
@@ -29,6 +41,18 @@
     phpinfo(INFO_MODULES);
 
     ?>
+
+    <!-- back-to-top section -------------->
+    <a href="#content" class="back-to-top">
+        <span>Retour en haut</span>
+        <!-- aria-hidden="true" opur masquer l'icon de l'écran -->
+        <svg width="18" height="18" viewbox=" 0 0 24 24" aria-hidden="true">
+            <path d="M5 12h14" />
+            <path d="m12 5 7 7-7 7" />
+        </svg>
+
+        <!-- Script Section : Script pour activer le menu mobile -->
+        <script src="script.js"></script>
 
 </body>
 
