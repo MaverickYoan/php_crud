c0d3fe8057c5e820492377ae94927feb7ab69ce4
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-05-22 13:52:09 +0200

    table sql creation

--- src/connect.php
+++ src/connect.php
@@ -6,10 +6,11 @@ const PASSWORD = "test";
 const PORT = "5432";
 
 try {
-    //code...
+    //code pour connexion réussie...
     $db = new PDO("pgsql:host= " . SERVER_NAME . ";port=" . PORT . ";dbname=" . DB_NAME . ";", USERNAME, PASSWORD);
     echo "connexion réussie";
 } catch (PDOException $e) {
+    //code pour connexion nok...
     //throw $th;
     echo "échec de connexion : " . $e->getMessage() . "<br>";
 }
--- src/index.php
+++ src/index.php
@@ -1,22 +1,65 @@
+<?php
+require_once("connect.php");
+
+// * sql SELECT
+$sql = "SELECT * FROM interns";
+
+// * préparation de la requête sql
+$query = $db->prepare($sql);
+
+// * préparation de la requête sql
+
+// * exécution de la requête sql
+$query->execute();
+
+// * récupération des données de la requête sql
+$interns = $query->fetchAll(PDO::FETCH_ASSOC);
+
+// * afficher la table interns
+print_r($interns);
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
-
-<!-- * HEAD -->
+<!-- HEAD -->
 
 <head>
     <!-- // - 2 - META -->
     <meta charset=utf-8>
     <meta content="width=device-width,initial-scale=1" name=viewport>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="favicon.png" type="image/x-png">
     <title>php_crud</title><!-- TITRE -->
 </head>
 
-<!-- * BODY -->
+<!-- BODY -->
 
 <body id="content">
     <h1>test</h1>
     <p>créer table sql interns</p>
+
+    <h1 style="color:green; font-size:14px;">TABLE SQL interns</h1>
+
+    <table style="border:1px solid black;">
+        <thead style="border:1px solid black;">
+            <th style="border:1px solid black;">id</th>
+            <th style="border:1px solid black;">firstName</th>
+            <th style="border:1px solid black;">lastName</th>
+            <th style="border:1px solid black;">actions</th>
+        </thead>
+        <tbody style="border:1px solid black;">
+            <tr>
+                <td style="border:1px solid black;">0</td>
+                <td style="border:1px solid black;">yoan</td>
+                <td style="border:1px solid black; display: flex; justify-content: center;">dym</td>
+                <td style="border:1px solid black;">noAction</td>
+            </tr>
+        </tbody>
+    </table>
+
+
 
     <!-- NAVBAR -->
     <nav class="navbar">
@@ -34,11 +77,11 @@
     <?php
 
     // Affiche toutes les informations, comme le ferait INFO_ALL
-    phpinfo();
+    // phpinfo();
 
     // Affiche uniquement le module d'information.
     // phpinfo(8) fournirait les mêmes informations.
-    phpinfo(INFO_MODULES);
+    // phpinfo(INFO_MODULES);
 
     ?>
 
