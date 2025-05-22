abc67a67db17b1076d95718accd304353c263ecb
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-05-22 11:08:24 +0200

    pgadmin

--- src/index.php
+++ src/index.php
@@ -16,8 +16,19 @@
 
 <body>
     <h1>test</h1>
-    <?php phpinfos(); ?>
+    <p>créer table sql interns</p>
 
+
+    <?php
+
+    // Affiche toutes les informations, comme le ferait INFO_ALL
+    phpinfo();
+
+    // Affiche uniquement le module d'information.
+    // phpinfo(8) fournirait les mêmes informations.
+    phpinfo(INFO_MODULES);
+
+    ?>
 
 </body>
 
