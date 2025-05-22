c48afb1d0ef596a0590b391c8d2d8c9fd96e18f2
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-05-22 10:40:20 +0200

    fonctionnel

--- src/index.php
+++ src/index.php
@@ -1,13 +1,3 @@
-<!-- + LEGENDE DES COMMENTAIRES -->
-
-<!-- COMMENTAIRES -->
-<!-- * OK -->
-<!-- // POUR INFOS -->
-<!-- ? ATTENTION -->
-<!-- TODO A FAIRE -->
-<!-- ! ERREURS A CORRIGER -->
-<!-- NEXT PROCHAINE ETAPE -->
-
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -17,34 +7,17 @@
 <head>
     <!-- // - 2 - META -->
     <meta charset=utf-8>
-    <meta content="IE=edge" http-equiv=X-UA-Compatible>
     <meta content="width=device-width,initial-scale=1" name=viewport>
-    <meta name="keywords" content="Voyages, Destinations, Tibet, Seychelles">
-    <meta name="description" content="Author=Flo, Yo">
 
-    <!-- // - 2 - LINK -->
-    <link rel="icon" href="/images/logo.png" type="image/x-icon">
-    <!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />-->
-
-    <link rel="xsl:stylesheet" href="xml/GlobeTrotters.xml">
-    <title>GlobeTrotters - À propos</title><!-- TITRE -->
+    <title>php_crud</title><!-- TITRE -->
 </head>
 
 <!-- * BODY -->
 
-<body id="content">
-    <div id="app"></div>
-    <script src="src/main.ts" type="module"></script>
-    <script src="script/fadein.js"></script>
+<body>
+    <h1>test</h1>
+    <?php phpinfos(); ?>
 
-    <!-- back-to-top section -------------->
-    <a href="#content" class="back-to-top">
-        <span>top</span>
-        <!-- aria-hidden="true" opur masquer l'icon de l'écran -->
-        <svg width="10" height="10" viewbox=" 0 0 24 24" aria-hidden="true">
-            <path d="M5 12h14" />
-            <path d="m12 5 7 7-7 7" />
-        </svg>
 
 </body>
 
